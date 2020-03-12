92c6c893e31d7df1b30247e68cf8a503eb0ddafb
Paolla Dutra <paolladutra@nyu.edu>
2020-03-11 20:22:37 -0400

    boolq file type

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -528,11 +528,11 @@ class BoolqProcessor(DataProcessor):
 
     def get_train_examples(self, data_dir):
         """See base class."""
-        return self._create_examples(self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "train.jsonl")), "train")
 
     def get_dev_examples(self, data_dir):
         """See base class."""
-        return self._create_examples(self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "dev.jsonl")), "dev")
 
     def get_labels(self):
         """See base class."""
